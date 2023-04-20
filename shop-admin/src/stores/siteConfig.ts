d47465f1bbade3752b741655cfc777b0ef6edc7d
my_god_404 <17679068837@163.com>
2023-04-20 10:18:58 +0800

    商户管理-web端

---
+++ shop-admin/src/stores/siteConfig.ts
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia'
+import { SiteConfig } from '/@/stores/interface'
+
+export const useSiteConfig = defineStore('siteConfig', {
+    state: (): SiteConfig => {
+        return {
+            siteName: '',
+            shopName: '',
+            shopLogo: '',
+            recordNumber: '',
+            version: '',
+            cdnUrl: '',
+            apiUrl: '',
+            upload: {
+                mode: 'local',
+                maxsize: 0,
+                mimetype: '',
+                savename: '',
+            },
+        }
+    },
+    actions: {
+        dataFill(state: SiteConfig) {
+            this.$state = state
+        },
+    },
+})
